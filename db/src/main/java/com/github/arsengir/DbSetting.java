b217f8f8df82bb6a4d63e6832d656664986ac8cd
Arsen Girinov <amateurlib@yandex.ru>
2021-03-16 22:32:50 +0300

    Task2

---
+++ db/src/main/java/com/github/arsengir/DbSetting.java
@@ -0,0 +1,13 @@
+package com.github.arsengir;
+
+public class DbSetting {
+
+    private String name;
+    private String password;
+
+    public DbSetting(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+}
